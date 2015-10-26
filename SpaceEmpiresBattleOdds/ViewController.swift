f763336291b564db6e421b2efce58be0e4477986
tiborgrose <tiborg@mac.com>
2015-10-26 23:44:58 +0900

    Initial Commit

---
+++ SpaceEmpiresBattleOdds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceEmpiresBattleOdds
+//
+//  Created by Tibor Grose on 10/26/15.
+//  Copyright © 2015 Tibor Grose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
